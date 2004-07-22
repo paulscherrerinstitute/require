d83241ac4e69cf954cb33ffd50b36d2e7d2ca063
maden <maden>
2004-07-22 14:25:30 +0000

    Add -h option.

--- App/scripts/iocBootNotify.sh
+++ App/scripts/iocBootNotify.sh
@@ -7,14 +7,31 @@
 if [ "$1" = "-v" ]
 then
     echo '$Source: /cvs/G/DRV/misc/App/scripts/iocBootNotify.sh,v $'
-    echo '$Author: zimoch $'
-    echo '$Date: 2004/05/03 12:59:56 $'
+    echo '$Author: maden $'
+    echo '$Date: 2004/07/22 14:25:30 $'
     exit
 fi
 
-if [ $# -lt 9 ]
+if [ "$1" = "-h" ] || [ "$1" = "-?" ]
+then
+    echo "Usage:" >&2
+    echo "        iocBootNotify.sh       \\" >&2
+    echo "                  <system>     \\" >&2
+    echo "                  <ipaddr>     \\" >&2
+    echo "                  <procnum>    \\" >&2
+    echo "                  <device>     \\" >&2
+    echo "                  <bootfile>   \\" >&2
+    echo "                  <script>     \\" >&2
+    echo "                  <vxworksver> \\" >&2
+    echo "                  <epicsver>   \\" >&2
+    echo "                  <ethaddr>"       >&2
+    exit 0
+fi
+
+if [ $# -lt 9 ] || [ "$1" = "-h" ] || [ "$1" = "-?" ]
 then
     echo "This script should only be called by an IOC at boot time!" >&2
+    echo "It needs 9 arguments." >&2
     exit 1
 fi
 
@@ -66,5 +83,17 @@ echo "EPICSVER=$EPICSVER"
 echo "VXWORKSVER=$VXWORKSVER"
 echo "ETHADDR=$ETHADDR"
 
-$SLSBASE/sls/bin/call_ioc_ins $SYSTEM $IPADDR $PROCNUM $DEVICE $BOOTPC $SLSBASE $BOOTFILE $SCRIPT $VXWORKS $EPICSVER $VXWORKSVER $ETHADDR
-
+$SLSBASE/sls/bin/call_ioc_ins $SYSTEM $IPADDR $PROCNUM $DEVICE \
+             $BOOTPC $SLSBASE $BOOTFILE $SCRIPT $VXWORKS $EPICSVER \
+             $VXWORKSVER $ETHADDR
+exit
+#--------------------------------------------------#
+# emacs setup - force text mode to prevent emacs   #
+#               from helping with the indentation! #
+# Local Variables:                                 #
+# mode:text                                        #
+# indent-tabs-mode:nil                             #
+# End:                                             #
+#--------------------------------------------------#
+#
+#---------------------------------------- End of $RCSfile: iocBootNotify.sh,v $
