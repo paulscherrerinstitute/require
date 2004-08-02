d923c1645290b9bf88aa313bc47730fe53848753
zimoch <zimoch>
2004-08-02 11:29:58 +0000

    use sqlplus directly instead of web server

--- App/scripts/iocBootNotify.sh
+++ App/scripts/iocBootNotify.sh
@@ -7,35 +7,28 @@
 if [ "$1" = "-v" ]
 then
     echo '$Source: /cvs/G/DRV/misc/App/scripts/iocBootNotify.sh,v $'
-    echo '$Author: maden $'
-    echo '$Date: 2004/07/22 14:25:30 $'
+    echo '$Author: zimoch $'
+    echo '$Date: 2004/08/02 11:29:58 $'
     exit
 fi
 
 if [ "$1" = "-h" ] || [ "$1" = "-?" ]
 then
-    echo "Usage:" >&2
-    echo "        iocBootNotify.sh       \\" >&2
-    echo "                  <system>     \\" >&2
-    echo "                  <ipaddr>     \\" >&2
-    echo "                  <procnum>    \\" >&2
-    echo "                  <device>     \\" >&2
-    echo "                  <bootfile>   \\" >&2
-    echo "                  <script>     \\" >&2
-    echo "                  <vxworksver> \\" >&2
-    echo "                  <epicsver>   \\" >&2
-    echo "                  <ethaddr>"       >&2
+{
+    echo "Usage:"
+    echo "        iocBootNotify.sh       \\"
+    echo "                  <system>     \\"
+    echo "                  <ipaddr>     \\"
+    echo "                  <procnum>    \\"
+    echo "                  <device>     \\"
+    echo "                  <bootfile>   \\"
+    echo "                  <script>     \\"
+    echo "                  <vxworksver> \\"
+    echo "                  <epicsver>   \\"
+    echo "                  <ethaddr>"
+} >&2
     exit 0
 fi
-
-if [ $# -lt 9 ] || [ "$1" = "-h" ] || [ "$1" = "-?" ]
-then
-    echo "This script should only be called by an IOC at boot time!" >&2
-    echo "It needs 9 arguments." >&2
-    exit 1
-fi
-
-#. /etc/profile
 
 SYSTEM=$1
 IPADDR=$2
@@ -46,21 +39,41 @@ SCRIPT=$6
 VXWORKSVER=${7#VxWorks}
 EPICSVER=$8
 ETHADDR=$9
+
+if [ $# -lt 9 ]
+then
+{
+    echo "This script should only be called by an IOC at boot time!"
+    echo "It needs 9 arguments."
+    echo "got: SYSTEM=\"$SYSTEM\""
+    echo "     IPADDR=\"$IPADDR\""
+    echo "     PROCNUM=\"$PROCNUM\""
+    echo "     DEVICE=\"$DEVICE\""
+    echo "     BOOTFILE=\"$BOOTFILE\""
+    echo "     SCRIPT=\"$SCRIPT\""
+    echo "     VXWORKSVER=\"$VXWORKSVER\""
+    echo "     EPICSVER=\"$EPICSVER\""
+    echo "     ETHADDR=\"$ETHADDR\""
+}  >&2
+    exit 1
+fi
+
+BOOTPC=$(hostname -s)
+
 if [ ! -L /ioc/$SYSTEM ]
 then
-  echo "ERROR: $SYSTEM is not an existing system name."
+  echo "ERROR: No symbolic link /ioc/$SYSTEM on $BOOTPC."
   echo "Rename 'target name' to your system name!"
   exit 1
 fi
 case $SYSTEM in
-    ( *-VME-* ) ;;
+    ( *-VME* ) ;;
     ( * ) echo "ERROR: $SYSTEM is not an acceptable system name."
-          echo "Rename your system and 'target name' to match *-VME-*."
+          echo "Rename your system and 'target name' to match *-VME*."
           exit 1 ;;
 esac
 link=$(readlink /ioc/$SYSTEM)
 SLSBASE=${link%%/iocBoot*}
-BOOTPC=$(hostname -s)
 if [ -L $BOOTFILE ]
 then
   link=$(readlink $BOOTFILE)
@@ -69,6 +82,7 @@ else
   VXWORKS=$BOOTFILE
 fi
 
+echo "New Version"
 echo "I will put the following values to the database:"
 echo "SYSTEM=$SYSTEM"
 echo "IPADDR=$IPADDR"
@@ -83,17 +97,24 @@ echo "EPICSVER=$EPICSVER"
 echo "VXWORKSVER=$VXWORKSVER"
 echo "ETHADDR=$ETHADDR"
 
-$SLSBASE/sls/bin/call_ioc_ins $SYSTEM $IPADDR $PROCNUM $DEVICE \
-             $BOOTPC $SLSBASE $BOOTFILE $SCRIPT $VXWORKS $EPICSVER \
-             $VXWORKSVER $ETHADDR
-exit
-#--------------------------------------------------#
-# emacs setup - force text mode to prevent emacs   #
-#               from helping with the indentation! #
-# Local Variables:                                 #
-# mode:text                                        #
-# indent-tabs-mode:nil                             #
-# End:                                             #
-#--------------------------------------------------#
-#
-#---------------------------------------- End of $RCSfile: iocBootNotify.sh,v $
+if [ -d /usr/oracle-9.2 ] ; then
+        export ORACLE_HOME=/usr/oracle-9.2
+else
+        export ORACLE_HOME=/usr/oracle-8.1.7
+	export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+fi
+
+$ORACLE_HOME/bin/sqlplus -s ssrm_public/pub01@psip0 << EOF
+INSERT INTO SSRM.IOC_BOOTLOG
+       (SYSTEM, IPADDR, PROCNUM, DEVICE, BOOTPC,
+        SLSBASE, BOOTFILE, SCRIPT, VXWORKS, EPICSVER,
+        VXWORKSVER, ETHADDR)
+VALUES ('$SYSTEM', '$IPADDR', '$PROCNUM', '$DEVICE', '$BOOTPC',
+        '$SLSBASE', '$BOOTFILE', '$SCRIPT', '$VXWORKS', '$EPICSVER',
+        '$VXWORKSVER', '$ETHADDR')
+EXIT
+EOF
+# $Name:  $
+# $Id: iocBootNotify.sh,v 1.5 2004/08/02 11:29:58 zimoch Exp $
+# $Source: /cvs/G/DRV/misc/App/scripts/iocBootNotify.sh,v $
+# $Revision: 1.5 $
