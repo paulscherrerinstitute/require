cd5457891a14aa16deb8c92c02643a6219753c8e
Dirk Zimoch <dirk.zimoch@psi.ch>
2015-12-03 16:54:30 +0100

    bugfix: require db dir is not in EPICS_DB_INCLUDE_PATH because require is not loaded with require

--- postModuleLoad.cmd
+++ postModuleLoad.cmd
@@ -1,1 +1,1 @@
-dbLoadRecords "moduleversion.template","IOC=$(IOC),MODULE=$(MODULE),VERSION=$($(MODULE)_VERSION),MODULE_COUNT=$(MODULE_COUNT),BUFFER_SIZE=$(BUFFER_SIZE)"
+dbLoadRecords "$(require_DIR)/db/moduleversion.template","IOC=$(IOC),MODULE=$(MODULE),VERSION=$($(MODULE)_VERSION),MODULE_COUNT=$(MODULE_COUNT),BUFFER_SIZE=$(BUFFER_SIZE)"
