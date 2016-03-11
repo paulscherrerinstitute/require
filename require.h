1bd82bad1d170890ec5d355f1839865a0be40e99
Dirk Zimoch <dirk.zimoch@psi.ch>
2016-03-11 16:48:33 +0100

    always print module location

--- require.h
+++ require.h
@@ -12,7 +12,7 @@ extern "C" {
 int require(const char* libname, const char* version, const char* args);
 const char* getLibVersion(const char* libname);
 const char* getLibLocation(const char* libname);
-int libversionShow(int showLocation, const char* outfile);
+int libversionShow(const char* outfile);
 int runScript(const char* filename, const char* args);
 int putenvprintf(const char* format, ...) __attribute__((format(printf,1,2)));
 
