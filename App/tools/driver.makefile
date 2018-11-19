paulscherrerinstitute/require

commit 0c827550130e2304f64b935cebbc5bff4b33a787

For newer compilers (not vxWorks 5) don't include system headers in .d files in order to avoid problems when cross-compiling for older Linux hosts

App/tools/driver.makefile
@@ -680,14 +680,19 @@ endif # MINOR
 LDFLAGS += ${PROVIDES} ${USR_LDFLAGS_${T_A}}
 
 # Create and include dependency files.
-# 3.14.8 uses HDEPENDS to select depends mode
-# 3.14.12 uses 'HDEPENDSCFLAGS -MMD' (does not catch #include <...>)
-# 3.15 uses 'HDEPENDS_COMPFLAGS = -MM -MF $@' (does not catch #include <...>)
+# 3.13 does not make those files at all.
+# 3.14.8 uses HDEPENDS to select depends mode.
+# 3.14.12 uses 'HDEPENDSCFLAGS -MMD'
+# 3.15 uses 'HDEPENDS_COMPFLAGS = -MM -MF $@'
+# For newer compilers they are ok and ignore files in system directories.
+# For old vxWorks gcc those rules ignore #include <...>,
+# which may be falsey used for non-system headers.
+ifneq (,$(filter T2-%,$(T_A)))
 HDEPENDS = 
 HDEPENDS_METHOD = COMP
 HDEPENDS_COMPFLAGS = -c
-#MKMF = DO_NOT_USE_MKMF
 CPPFLAGS += -MD
+endif
 -include *.d
 
 # Need to find source dbd files relative to one dir up but generated dbd files in this dir.

commit 120c1e65ea804e1a642c2b1c33e96139c9875e2c

commit 0c82755 broke creation of dependency files for 3.14.8

App/tools/driver.makefile
@@ -694,14 +694,16 @@ endif # !USE_EXACT_VERSION
 
 # Create and include dependency files.
 # 3.13 does not make those files at all.
-# 3.14.8 uses HDEPENDS to select depends mode.
+# 3.14.8 uses HDEPENDS=YES to select depends mode.
 # 3.14.12 uses 'HDEPENDSCFLAGS -MMD'
 # 3.15 uses 'HDEPENDS_COMPFLAGS = -MM -MF $@'
 # For newer compilers they are ok and ignore files in system directories.
 # For old vxWorks gcc those rules ignore #include <...>,
 # which may be falsey used for non-system headers.
+ifdef HDEPENDS
+CPPFLAGS += -MMD
+endif
 ifneq (,$(filter T2-%,$(T_A)))
-HDEPENDS = 
 HDEPENDS_METHOD = COMP
 HDEPENDS_COMPFLAGS = -c
 CPPFLAGS += -MD